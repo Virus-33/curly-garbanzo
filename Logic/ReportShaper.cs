8756c7696f8d51c1ee42d4603c8b7158107e0537
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2024-02-17 12:47:37 +0700

    Inittialization commit

---
+++ Logic/ReportShaper.cs
@@ -0,0 +1,29 @@
+﻿namespace Logic
+{
+    enum GroupType
+    {
+        absentia,
+        intramural
+    }
+
+    enum WorkType
+    {
+        lecture,
+        practiceWork,
+        lab,
+        consultation,
+        unmarkedExam,
+        markedExam,
+        courseProject,
+        CGW,
+        practice,
+        graduationWork,
+        GEC,
+        magistracyLeader,
+        internLeader
+    }
+
+    public class ReportShaper
+    {
+    }
+}
